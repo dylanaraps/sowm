363ad293e45c8afdffc17a76b96dc61c4664ec63
pyknite <julien.rinaldini@heig-vd.ch>
2010-02-22 16:15:28 +0100

    add configuration file

---
+++ config.h
@@ -0,0 +1,60 @@
+/*
+ *   /\___/\
+ *  ( o   o )  Made by cat...
+ *  (  =^=  )
+ *  (        )            ... for cat!
+ *  (         )
+ *  (          ))))))________________
+ *  ______________________________________________________________________________
+ *
+ *  Copyright (c) 2010, Rinaldini Julien, julien.rinaldini@heig-vd.ch
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Mod (Mod1 == alt) and master size
+#define MOD             Mod1Mask
+#define MASTER_SIZE     0.7
+
+// Colors
+#define FOCUS           "rgb:255/0/0"
+#define UNFOCUS         "rgb:0/255/0"
+
+const char* dmenucmd[] = {"dmenu_run",NULL};
+const char* urxvtcmd[] = {"urxvt",NULL};
+
+// Shortcuts
+struct key keys[] = {
+    // MOD              KEY         FUNCTION        ARGS
+    {  MOD,             XK_q,       quit,           NULL},
+    {  MOD,             XK_p,       spawn,          dmenucmd},
+    {  MOD|ShiftMask,   XK_Return,  spawn,          urxvtcmd},
+    {  MOD,             XK_t,       test,           NULL}
+};
+
+#endif
+
