dc5742150a34c72403c734b1ee78475bc777a1e1
Dylan Araps <dylan.araps@gmail.com>
2019-10-11 20:06:19 +0300

    catwm: Added center_window

--- config.h
+++ config.h
@@ -4,29 +4,8 @@
  *  (  =^=  )
  *  (        )            ... for cat!
  *  (         )
- *  (          ))))))________________ Cute And Tiny Window Manager
- *  ______________________________________________________________________________
- *
- *  Copyright (c) 2010, Rinaldini Julien, julien.rinaldini@heig-vd.ch
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a
- *  copy of this software and associated documentation files (the "Software"),
- *  to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the
- *  Software is furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- *  DEALINGS IN THE SOFTWARE.
- *
+ *  (          ))))))________________
+ *  __________________
  */
 
 #ifndef CONFIG_H
@@ -54,6 +33,7 @@ const char* volup[]    = {"amixer","sset","Master","5\%+",NULL};
 static struct key keys[] = {
     // MOD              KEY                         FUNCTION        ARGS
     {  MOD,             XK_q,                       kill_client,    {NULL}},
+    {  MOD,             XK_c,                       center_window,  {NULL}},
     {  Mod1Mask,        XK_Tab,                     next_win,       {NULL}},
     {  0,               XF86XK_AudioLowerVolume,    spawn,          {.com = voldown}},
     {  0,               XF86XK_AudioRaiseVolume,    spawn,          {.com = volup}},
