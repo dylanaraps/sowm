04b35c3f5e8b13d50ad4db361efebda9a939831e
Dylan Araps <dylan.araps@gmail.com>
2020-07-11 11:45:17 +0300

    sowm.h: Fix mixed indentation

--- sowm.h
+++ sowm.h
@@ -2,12 +2,12 @@
 #include <xcb/xcb.h>
 
 struct wconf {
-	int16_t x;
+    int16_t x;
     int16_t y;
-	uint16_t width;
+    uint16_t width;
     uint16_t height;
-	uint8_t stack_mode;
-	xcb_window_t sibling;
+    uint8_t stack_mode;
+    xcb_window_t sibling;
 };
 
 xcb_connection_t *dpy;
